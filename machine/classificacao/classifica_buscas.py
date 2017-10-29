579d0f1e994546bbd885800a94d316ab56fe1c03
valdeco <valdemirdefranca@gmail.com>
2017-10-29 20:58:08 -0200

    cap 4 video 6

--- machine/classificacao/classifica_buscas.py
+++ machine/classificacao/classifica_buscas.py
@@ -1,4 +1,6 @@
+from collections import Counter
 import pandas as pd
+
 df = pd.read_csv('busca.csv')
 
 X_df = df[['home','busca', 'logado']]
@@ -9,6 +11,11 @@ Ydummies_df = Y_df
 
 X = Xdummies_df.values
 Y = Ydummies_df.values
+
+acerto_base = max(Counter(Y).values())
+
+taxa_de_acerto_base = 100.0 * acerto_base/len(Y)
+print("Taxa de acerto base: %f" % taxa_de_acerto_base)
 
 porcentagem_de_treino = 0.9
 
@@ -26,15 +33,15 @@ modelo = MultinomialNB()
 modelo.fit(treino_dados, treino_marcacoes)
 
 resultado = modelo.predict(teste_dados)
-diferencas = resultado - teste_marcacoes
+acertos = (resultado == teste_marcacoes)
 
-acertos = [d for d in diferencas if d==0]
-total_de_acertos = len(acertos)
+
+total_de_acertos = sum(acertos)
 total_de_elementos = len(teste_dados)
 
 taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
 
-print (taxa_de_acerto)
+print ("Taxa de acerto do algoritmo: %f" % taxa_de_acerto)
 print (total_de_elementos)
 
 
